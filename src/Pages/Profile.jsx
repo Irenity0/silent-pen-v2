df6ac0137b3bac8c98be71a05656055a14a2e69b
Irenity <lilithkard77@gmail.com>
2025-01-19 16:07:13 +0600

    project done (for now)

--- src/Pages/Profile.jsx
+++ src/Pages/Profile.jsx
@@ -3,9 +3,11 @@ const Profile = () => {
         <section className="w-10/12 md:11/12 lg:w-1/2 mx-auto">
         <h1 className="text-xl text-accent font-semibold">About Me</h1>
         <br />
+        <div className="text-accent text-lg font-newsreader">
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dicta unde nihil enim recusandae et, nam iste, soluta debitis maiores facilis nobis eaque sit, aspernatur natus atque ratione animi sunt? Corporis!</p>
         <br />
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et dolor repellat quisquam, eaque molestias sit soluta, nam voluptas placeat omnis quidem magni ut quas dolorum, ipsum recusandae quibusdam assumenda repudiandae?</p>
+        </div>
 
         </section>
     );
